4b113362428c1ce8caa540f643aada83022e0e5d
alishalisha <alisha.ramos@voxmedia.com>
2015-08-26 16:31:40 -0400

    sending data to google

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 var refURL = '';
 
-// $.each(environmentData, function(index, value){
-//   var obj = value;
-//   // find each field and prepopulate it with the value
-//   if (obj.value) {
-//     $(obj.id).attr('value', obj.value);
-//   };
-//   if (obj.env == true) {
-//     $('#browser-info').append(obj.markup + obj.value + '<br />');
-//   }
-// });
+var data = ['.q1', '.q2', '.q3', '.q4', '.q5', '.q6', '.q7'];
 
 // when clicking other, enable the textarea.
 $textarea = $('.ad-survey__form--option--text');
@@ -25,17 +16,21 @@ $other.on('click', function() {
 });
 
 // Form submission
-$('#ad-report-form').one('submit',function(){
-  $.each(environmentData, function(index, value){
-    var obj = value;
-    // find the value of each field and encode it
-    var encodedValue = encodeURIComponent($(obj.id).val());
-    var ref = '&entry.' + obj.entryID + '=' + encodedValue;
-    refURL = refURL + ref;
+$('#ad-survey-form').one('submit',function(){
+  $.each(data, function(i){
+    var isChecked = $(data[i]).find('input').prop('checked');
+    var optionText = $(data[i]).find('label').html();
+
+    if (isChecked == true) {
+      var encodedValue = encodeURIComponent(optionText);
+      var ref = '&entry.1999326285=' + encodedValue;
+      refURL = refURL + ref;
+    }
   });
-  var baseURL = 'https://docs.google.com/a/voxmedia.com/forms/d/1OYouWfgdIPXOBBsoLznAawKYdJ9tjEz_Tccb8dFpJjg/formResponse?';
+  console.log(refURL);
+  var baseURL = 'https://docs.google.com/a/voxmedia.com/forms/d/1u7JK3TY7L3hV0BqO-ZffKwUS7JwbQt1ONBC4xJbbzCs/formResponse?';
   var submitRef = '&submit=submit';
   var submitURL = baseURL + refURL + submitRef;
   $(this)[0].action=submitURL;
-  $('body').append('<h3>Thank You!</h3>');
+  $('.ad-survey__form').append('<h3>Thank You!</h3>');
 });
